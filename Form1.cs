fec13b35e876c16cdbb09fc669726d32ba84df12
carlos martins <carlosmartins.13046@outlook.com>
2023-12-13 15:48:47 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace EX_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Utilizador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_17
+{
+    internal class Utilizador
+    {
+        private static string nome;
+        private static string Email;
+        private static string Password;
+
+    }
+}
